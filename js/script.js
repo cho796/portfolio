99abfa1b07c55edd66ba13a58bc137144c07ff30
cho796 <gdr6.d.c.cho@gmail.com>
2026-05-28 22:50:43 +0900

    update link

--- js/script.js
+++ js/script.js
@@ -21,28 +21,28 @@ new jBox('Modal', {
     height: 80,
     attach: '#my-modal-pulseHorizon',
     title: 'PC版またはスマホ版を選んで下さい',
-    content: '<a href="https://drive.google.com/file/d/1BEoCoFNzkTgA5SaHwn6uzJcQuxkRvexT/view?usp=sharing"><i class="fa-solid fa-computer"></i><br>PC</a><a href="https://drive.google.com/file/d/1n8hPLozZYwFSy8IKQtYk2dxkmizTA4UU/view?usp=sharing"><i class="fa-solid fa-mobile-screen-button"></i><br>スマホ</a>'
+    content: '<a href="https://www.dropbox.com/scl/fi/5w4tp47jcijmhx10awlp7/pc.pdf?rlkey=1yawepi61p4sass1r7x0qu8qt&st=pl1pju57&dl=0"><i class="fa-solid fa-computer"></i><br>PC</a><a href="https://www.dropbox.com/scl/fi/2dp72d4px07zzqwjozcz9/sp.pdf?rlkey=gtjz8y9b5h2pgz4u4q4y41lrx&st=qcd4e9dm&dl=0"><i class="fa-solid fa-mobile-screen-button"></i><br>スマホ</a>'
 });
 new jBox('Modal', {
     width: 305,
     height: 80,
     attach: '#my-modal-chiharadai',
     title: 'PC版またはスマホ版を選んで下さい',
-    content: '<a href="https://drive.google.com/file/d/1GWDTyqp2PLVPEDdWqooSzuUGHmUvox9N/view?usp=sharing"><i class="fa-solid fa-computer"></i><br>PC</a><a href="https://drive.google.com/file/d/1w50_RGna7hmll_7u6UZsB1DJ8FHGtxtv/view?usp=sharing"><i class="fa-solid fa-mobile-screen-button"></i><br>スマホ</a>'
+    content: '<a href="https://www.dropbox.com/scl/fi/mxahxi6iuwprsbr3rnvlv/pc.pdf?rlkey=2zpu2xck4o9tqeyyxgtpf6hlv&st=nyqiwulg&dl=0"><i class="fa-solid fa-computer"></i><br>PC</a><a href="https://www.dropbox.com/scl/fi/2ekbh3zhi2ku0zq75f7rc/sp.pdf?rlkey=06ly738th00lfouktb8uomz0u&st=cfwnkh4r&dl=0"><i class="fa-solid fa-mobile-screen-button"></i><br>スマホ</a>'
 });
 new jBox('Modal', {
     width: 305,
     height: 80,
     attach: '#my-modal-yoshidaShikaiin',
     title: 'PC版またはスマホ版を選んで下さい',
-    content: '<a href="https://drive.google.com/file/d/10UsyweEJ-9qixNvbjcHYTCRlmMX77RQz/view?usp=sharing"><i class="fa-solid fa-computer"></i><br>PC</a><a href="https://drive.google.com/file/d/1XbHLEt7bsAsmOEfZKnDOBnEIB3PK13Pp/view?usp=sharing"><i class="fa-solid fa-mobile-screen-button"></i><br>スマホ</a>'
+    content: '<a href="https://www.dropbox.com/scl/fi/xsk2zmw6715n7pwyanao5/pc.pdf?rlkey=8xkl33owyxoaw2c494ft8b2sc&st=dnqr0uju&dl=0"><i class="fa-solid fa-computer"></i><br>PC</a><a href="https://www.dropbox.com/scl/fi/xkz8i43sp3apml3l734aq/sp.pdf?rlkey=p9fek57uoz4xpzgqx8rdk47yh&st=cooreimv&dl=0"><i class="fa-solid fa-mobile-screen-button"></i><br>スマホ</a>'
 });
 new jBox('Modal', {
     width: 305,
     height: 80,
     attach: '#my-modal-wetWipes',
     title: 'PC版またはスマホ版を選んで下さい',
-    content: '<a href="https://drive.google.com/file/d/1kim5oKOtF_Tb1NjAmVd4Hw1LfFZ7JRoL/view?usp=sharing"><i class="fa-solid fa-computer"></i><br>PC</a><a href="https://drive.google.com/file/d/1kgJA84Ocqbn8QeF7jQIZYautaJXs6ZL4/view?usp=sharing"><i class="fa-solid fa-mobile-screen-button"></i><br>スマホ</a>'
+    content: '<a href="https://www.dropbox.com/scl/fi/o97njeg282ro87r191eli/pc.pdf?rlkey=skbnbrk2nus4bw36t3ttjkdee&st=qbf2tgqs&dl=0"><i class="fa-solid fa-computer"></i><br>PC</a><a href="https://www.dropbox.com/scl/fi/npl14fecayln2y27j0xpv/sp.pdf?rlkey=ifkjb88nv8j5javojqlqaf5h7&st=kr6en1r6&dl=0"><i class="fa-solid fa-mobile-screen-button"></i><br>スマホ</a>'
 });
 
 AOS.init({
